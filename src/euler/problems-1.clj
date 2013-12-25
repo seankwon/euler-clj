b6cb3e86bf738f04bf158dd413d3b07a8d497a3f
Sean Kwon <seankwon@seankwon-Lenovo-G505.(none)>
2013-12-25 00:52:54 -0500

    cleaning input for problem 4

--- src/euler/problems-1.clj
+++ src/euler/problems-1.clj
@@ -1,12 +1,15 @@
 (ns euler.problems-1)
 
 ;; problem 2
+
 (def even-fibs-sum-seq-four-mil
   (let [fib       (fn [x y]
                     (lazy-seq (cons y (fib y (+ x y)))))
         even-fibs (filter even? (fib 1 1))]
     (reduce + (take-while #(< % 4000000) even-fibs))))
+
 ;;problem 3, learn and reread
+
 (def largest-prime [n]
   (let [is-prime? (fn [x]
                     (zero? (count (filter
@@ -15,27 +18,28 @@
     (first (filter #(is-prime? %)
                    (filter #(zero? (rem n %))
                            (reverse (range 3 big-number 2)))))))
-;;problem 4
-(defn is-six-palindrome? [x]
-  (= (str (int (double (/ x 1000))))
-     (clojure.string/join (reverse (str (- x (* (int (/ x 1000)) 1000)))))))
 
-(def palindromes
-  (filter is-six-palindrome? (reverse (range 100000 999999))))
+;;problem 4 ultra messy, but eh it works.
 
-(def pos-threes
-  (range 100 999))
-
-(loop [palindrome palindromes pos-threes pos-threes]
-  (if (and
-       (and (not (zero? (count palindrome))) (not (zero? (count pos-threes))))
-       (zero? (rem (first palindrome) (first pos-threes))) 
-       (<= (count (str (/ (first palindrome) (first pos-threes)))) 3))
-    (first palindrome)
-    (recur (if (zero? (count pos-threes))
-             (rest palindrome)
-             palindrome)
-           (if (zero? (count pos-threes))
-             (reverse (range 100 999))
-             (rest pos-threes)))))
+(def palindromic-product 
+  (let [pos-threes (range 100 999)
+        is-six-palindrome (fn [x]
+                            (= (str (int (double (/ x 1000))))
+                               (clojure.string/join 
+                                 (reverse 
+                                    (str (- x (* (int (/ x 1000)) 1000)))))))
+        palindromes (filter is-six-palindrome? (reverse (range 100000 999999)))]
+    (loop [palindrome palindromes pos-threes pos-threes]
+      (if (and
+           (and (not (zero? (count palindrome))) (not (zero? (count pos-threes))))
+           (zero? (rem (first palindrome) (first pos-threes))) 
+           (<= (count (str (/ (first palindrome) (first pos-threes)))) 3))
+        (first palindrome)
+        (recur (if (zero? (count pos-threes))
+                 (rest palindrome)
+                 palindrome)
+               (if (zero? (count pos-threes))
+                 (reverse (range 100 999))
+                 (rest pos-threes)))))))
 ;;problem 5
+
---
+++ src/euler/problems-2.clj
@@ -0,0 +1,4 @@
+(ns euler.problems-2)
+
+(defn sum [x y]
+  (+ x y))
