91a3a8a6c4e00341371d3fd7424c3453c352df6e
Sean Kwon <seankwon@seankwon-Lenovo-G505.(none)>
2013-12-25 00:37:01 -0500

    complete problem 2 3 4

--- src/euler/problems-1.clj
+++ src/euler/problems-1.clj
@@ -6,3 +6,36 @@
                     (lazy-seq (cons y (fib y (+ x y)))))
         even-fibs (filter even? (fib 1 1))]
     (reduce + (take-while #(< % 4000000) even-fibs))))
+;;problem 3, learn and reread
+(def largest-prime [n]
+  (let [is-prime? (fn [x]
+                    (zero? (count (filter
+                                   #(zero? (rem n %)) (range 3 n 2)))))
+        big-number (Math/sqrt n)]
+    (first (filter #(is-prime? %)
+                   (filter #(zero? (rem n %))
+                           (reverse (range 3 big-number 2)))))))
+;;problem 4
+(defn is-six-palindrome? [x]
+  (= (str (int (double (/ x 1000))))
+     (clojure.string/join (reverse (str (- x (* (int (/ x 1000)) 1000)))))))
+
+(def palindromes
+  (filter is-six-palindrome? (reverse (range 100000 999999))))
+
+(def pos-threes
+  (range 100 999))
+
+(loop [palindrome palindromes pos-threes pos-threes]
+  (if (and
+       (and (not (zero? (count palindrome))) (not (zero? (count pos-threes))))
+       (zero? (rem (first palindrome) (first pos-threes))) 
+       (<= (count (str (/ (first palindrome) (first pos-threes)))) 3))
+    (first palindrome)
+    (recur (if (zero? (count pos-threes))
+             (rest palindrome)
+             palindrome)
+           (if (zero? (count pos-threes))
+             (reverse (range 100 999))
+             (rest pos-threes)))))
+;;problem 5
